a613053c111e83b4cc9499e666a0333b6f04b953
Ilya <veruhanov00@mail.ru>
2022-12-04 17:19:19 +0300

    add task lection3

---
+++ Desktop/java/lec3/Ex002_ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Ex002_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(2809 + 23432);
+
+        for (Object o : list){
+            System.out.println(o);
+        }
+    }
+}
